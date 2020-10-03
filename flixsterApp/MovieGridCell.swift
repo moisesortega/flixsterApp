c87370ea6b68c561fa97d51acf89c8464d525bf0
Moises <ortega.moises423@gmail.com>
2020-10-02 19:13:40 -0700

    Unit 2 addition

---
+++ flixsterApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixsterApp
+//
+//  Created by Moises Ortega on 10/2/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
